75e3fe41c227ff00ad4e16b801f882f1a91d8f13
atjapan2015 <atjapan2015>
2019-06-03 18:19:51 +0800

    update

--- src/main/resources/WEB/src/js/viewModels/customers.js
+++ src/main/resources/WEB/src/js/viewModels/customers.js
@@ -6,27 +6,127 @@
 /*
  * Your customer ViewModel code goes here
  */
-define(['ojs/ojcore', 'ojs/ojmodel', 'ojs/ojdatagrid', 'ojs/ojcollectiondatagriddatasource'],
- function(oj, Model) {
+define(['ojs/ojcore', 'ojs/ojmodel', 'ojs/ojvalidation-base', 'knockout', 'ojs/ojknockout', 'ojs/ojdatagrid', 'ojs/ojcollectiondatagriddatasource', 'ojs/ojinputtext', 'ojs/ojinputnumber', 'ojs/ojdatetimepicker','ojs/ojformlayout'],
+ function(oj, Model, Validation, ko) {
 
     function CustomerViewModel() {
       var self = this;
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
-	self.url = '/greet/employees';
 
-	self.collection = new oj.Collection(null, {
+		  var dateOptions = { formatType: 'date', dateFormat: 'medium' };
+          var dateConverterFactory = Validation.Validation.converterFactory("datetime");
+          self.dateConverter = dateConverterFactory.createConverter(dateOptions);
+  
+          var salaryOptions = {
+            style: "currency",
+            currency: "USD",
+            currencyDisplay: "symbol"
+          };
+          var salaryConverterFactory =
+            Validation.Validation.converterFactory("number");
+          self.salaryConverter =
+            salaryConverterFactory.createConverter(salaryOptions);
+
+	  self.url = '/greet/employees';
+
+	  self.collection = new oj.Collection(null, {
 	    model: new oj.Model.extend({
 		idAttribute: 'id',
 		urlRoot: self.url}),
 	    url: self.url
-	});
+	  });
 
-	self.dataSource = new oj.CollectionDataGridDataSource(
+	  self.dataSource = new oj.CollectionDataGridDataSource(
 	   self.collection, {
-	      rowHeader: 'id',
-	      columns: ['FIRST_NAME', 'LAST_NAME', 'HIRE_DATE', 'SALARY']
-	});
+	     rowHeader: 'id',
+	     columns: ['FIRST_NAME', 'LAST_NAME', 'HIRE_DATE', 'SALARY']
+	  });
+
+		var nextKey = 121;
+		self.inputEmployeeID = ko.observable(nextKey);
+		self.inputFirstName = ko.observable();
+		self.inputLastName = ko.observable();
+		self.inputHireDate = ko.observable();
+		self.inputSalary = ko.observable();
+		
+		//build a new model from the observables in the form
+          self.buildModel = function () {
+            return {
+              'EMPLOYEE_ID': self.inputEmployeeID(),
+              'FIRST_NAME': self.inputFirstName(),
+              'LAST_NAME': self.inputLastName(),
+              'HIRE_DATE': self.inputHireDate(),
+              'SALARY': self.inputSalary()
+            }
+          }.bind(this);
+		
+		//used to update the fields based on the selected row
+          self.updateFields = function (model) {
+            self.inputEmployeeID(model.get('EMPLOYEE_ID'));
+            self.inputFirstName(model.get('FIRST_NAME'));
+            self.inputLastName(model.get('LAST_NAME'));
+            self.inputHireDate(model.get('HIRE_DATE'));
+            self.inputSalary(model.get('SALARY'));
+          };
+  
+          //add the model to the collection at index 0
+          self.add = function () {
+            if (self.inputEmployeeID(nextKey) < nextKey) {
+              self.inputEmployeeID(nextKey);
+            }
+            var model = self.buildModel();
+            nextKey += 1;
+            self.inputEmployeeID(nextKey);
+            self.collection.add(model, { at: 0 });
+          }.bind(this);
+  
+          // update the model in the collection
+          self.update = function () {
+            if (self.modelToUpdate) {
+              self.modelToUpdate.set(self.buildModel());
+            }
+          }.bind(this);
+  
+          //remove the selected model from the collection
+          self.remove = function () {
+            self.collection.remove(self.modelToUpdate);
+          }.bind(this);
+  
+          //reset the fields to their original values
+          self.resetFields = function () {
+            self.inputEmployeeID(nextKey);
+            self.inputFirstName('Jane');
+            self.inputLastName('Doe');
+            self.inputHireDate(Validation.IntlConverterUtils.dateToLocalIso(new Date()));
+            self.inputSalary(15000);
+          }.bind(this);
+  
+          //intialize the observable values in the forms
+          self.inputEmployeeID = ko.observable(nextKey);
+          self.inputFirstName = ko.observable('Jane');
+          self.inputLastName = ko.observable('Doe');
+          self.inputHireDate = ko.observable(Validation.IntlConverterUtils.dateToLocalIso(new Date()));
+          self.inputSalary = ko.observable(15000);
+  
+          self.getCellClassName = function (cellContext) {
+            var key = cellContext['keys']['column'];
+            if (key === 'SALARY') {
+              return 'oj-helper-justify-content-right';
+            }
+            return 'oj-helper-justify-content-flex-start';
+          }
+
+		self.handleSelectionChanged = function (event) {
+		     var selection = event.detail['value'][0];
+		     if (selection != null) { 
+		         var rowKey = selection['startKey']['row'];
+		         self.modelToUpdate = self.collection.get(rowKey);
+		         self.updateFields(self.modelToUpdate);
+		     }
+		};
+  
+     
 
       /**
        * Optional ViewModel method invoked after the View is inserted into the
